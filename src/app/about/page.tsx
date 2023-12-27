6373fa71dafafcaec1beb616aca3d9e02cbb48ad
Jasper Hsiao <orz2931@gmail.com>
2023-12-27 20:40:17 +0800

    new project

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
