a7fca0c83a90f55b57d803963aa5ce3fdbba6da9
Jasper Hsiao <orz2931@gmail.com>
2024-01-12 16:47:47 +0800

    new posts

--- src/app/components/navigation/navigation.tsx
+++ src/app/components/navigation/navigation.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import ThemeSwitch from "../themeswitch/themeswitch";
+
 export default function Navigation() {
   return (
     <div>
-      <ThemeSwitch />
       <Link href={"/"}>Home</Link>
       <Link href={"/about"}>About</Link>
       <Link href={"/blog"}>Blog</Link>
+      <ThemeSwitch />
     </div>
   );
 }
